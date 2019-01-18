eee2c3f0af48b13aac7daaceed5d6ad229fae3fd
Tayfun Ateş <tayfun.ates@avcrtech.com>
2019-01-18 09:46:48 +0300

    Model updates with bn and relu layers.

--- src/ReidRNSelf.py
+++ src/ReidRNSelf.py
@@ -11,13 +11,19 @@ class FCOutputModel(nn.Module):
         super(FCOutputModel, self).__init__()
 
         self.fc2 = nn.Linear(256, 256)
+        self.bn2 = nn.BatchNorm1d(256)
         self.fc3 = nn.Linear(256, 128)
+        self.bn3 = nn.BatchNorm1d(128)
 
     def forward(self, x):
         x = self.fc2(x)
+        x = self.bn2(x)
         x = F.relu(x)
         x = F.dropout(x)
         x = self.fc3(x)
+    	x = self.bn3(x)
+     	x = F.relu(x)
+
         return x
 
 
